0f4990e0aa01a655afe9a8dfd05e429040f3b25f
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-18 10:34:38 +0530

    feat: integrate Flowbite for enhanced UI components
    - Added Flowbite as a dependency in package.json.
    - Removed placeholder content from app.html and replaced it with a structured layout.
    - Initialized Flowbite in the App component's ngOnInit lifecycle method.
    - Updated global styles to include Flowbite's default theme and Inter font.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  ngOnInit(): void {
+    initFlowbite();
+  }
   protected readonly title = signal('ThogaKade-POS');
 }
