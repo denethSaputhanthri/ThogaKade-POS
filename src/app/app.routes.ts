9f03f6fcbef5d23c71a2110f029a50e1f931d7f5
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-18 11:19:27 +0530

    feat: implement dashboard structure with customer, item, and order components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './page/dashboard/dashboard';
+import { Customer } from './page/dashboard/customer/customer';
+import { Item } from './page/dashboard/item/item';
+import { Order } from './page/dashboard/order/order';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:Dashboard,
+    },
+    {
+        path:"dashboard",
+        component:Dashboard,
+        children:[
+            {
+                path:"customer",
+                component:Customer,
+            },
+            {
+                path:"item",
+                component:Item
+            },
+            {
+                path:"order",
+                component:Order
+            },
+        ],
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { Login } from "./page/login/login";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
