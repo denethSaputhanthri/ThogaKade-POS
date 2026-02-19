4565e98ad68117e76b06bd5c59f191fefc30a85c
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-19 11:35:29 +0530

    feat: enhance customer management with new fields and data fetching

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CustomerModel } from '../../../../model/type';
+import { HttpClient } from '@angular/common/http';
+import { NgForOf } from '@angular/common';
+
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class Customer {
+
+  customerList:Array<CustomerModel>=[];
+
+  constructor(private http:HttpClient){
+    this.loadTable();
+  }
+
+  loadTable(){
+    this.http.get<CustomerModel[]>(`http://localhost:8080/customer/get`).subscribe(data =>{
+      console.log(data);
+      
+      this.customerList=data
+    })
+  }
 
 }
---
+++ src/model/type.ts
@@ -0,0 +1,15 @@
+export interface CustomerModel{
+    id:string;
+    title:string;
+    name:string;
+    dobValue:Object;
+    salary:number;
+    address:string;
+    city:string;
+    province:string;
+    postalCode:string;
+}
+
+export interface ItemModel{
+
+}
