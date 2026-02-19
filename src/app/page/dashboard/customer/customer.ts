9cb926ca04f060f549ea322193f2775a434bf2c3
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-19 15:09:36 +0530

    feat: integrate SweetAlert2 for customer addition feedback and bind form inputs to customer object

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -2,32 +2,58 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerModel } from '../../../../model/type';
 import { HttpClient } from '@angular/common/http';
 import { NgForOf } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-customer',
-  imports: [NgForOf],
+  imports: [NgForOf, FormsModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class Customer implements OnInit {
+  customerList: Array<CustomerModel> = [];
 
-  customerList:Array<CustomerModel>=[];
+  customerObj: CustomerModel = {
+    id: '',
+    title: '',
+    name: '',
+    dobValue: '',
+    salary: 0.0,
+    address: '',
+    city: '',
+    province: '',
+    postalCode: '',
+  };
 
-  constructor(private http:HttpClient,private cdr:ChangeDetectorRef){
-  }
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef,
+  ) { }
   ngOnInit(): void {
     this.getAll();
   }
 
-
-
-  getAll(){
-    this.http.get<CustomerModel[]>(`http://localhost:8080/customer/get`).subscribe(data =>{
+  addCustomer() {
+    console.log(this.customerObj);
+    this.http.post(`http://localhost:8080/customer/add`, this.customerObj).subscribe((data) => {
       console.log(data);
-      this.customerList=data
-      this.cdr.detectChanges();
-    })
+      if (data === true) {
+        Swal.fire({
+          title: "Good job !"+this.customerObj.name+"saved!",
+          text: 'You clicked the button !',
+          icon: "success",
+        });
+      }
+      this.getAll();
+    });
   }
 
+  getAll() {
+    this.http.get<CustomerModel[]>(`http://localhost:8080/customer/get`).subscribe((data) => {
+      console.log(data);
+      this.customerList = data;
+      this.cdr.detectChanges();
+    });
+  }
 }
