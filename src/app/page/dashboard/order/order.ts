8c8691ba755d7b0588226ac75b21181c0825a967
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-20 19:58:12 +0530

    feat: add order ID input and update order management functionality

--- src/app/page/dashboard/order/order.ts
+++ src/app/page/dashboard/order/order.ts
@@ -2,15 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { OrderModel } from '../../../../model/type';
 import { NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
-  imports: [NgForOf],
+  imports: [NgForOf,FormsModule],
   templateUrl: './order.html',
   styleUrl: './order.css',
 })
 export class Order implements OnInit{
   orderList: Array<OrderModel>=[];
+
+  orderObj: OrderModel={
+    id: '',
+    orderDate: new Date(),
+    customerId: ''
+  }
 
   constructor(
     private http: HttpClient,
@@ -19,6 +26,16 @@ export class Order implements OnInit{
   
   ngOnInit(): void {
    this.getAll();
+  }
+
+  addOrder(){
+    this.http.post(`http://localhost:8080/order/add`,this.orderObj).subscribe
+    ((data)=>{
+      console.log(this.orderObj);
+    
+      
+    });
+    this.getAll()
   }
 
   getAll(){
